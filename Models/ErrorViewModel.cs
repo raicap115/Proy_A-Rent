5f413e4c451aa7b51735fd3cdb277f81761beea9
Raí Campos <raicampas23@gmail.com>
2020-11-25 14:20:49 -0500

    new mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Proy_A_Rent.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
