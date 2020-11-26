6fe21cebf3d212f7d2907fabffc0319ec52a38bb
Angie Ortiz <angieortizavalos@gmail.com>
2020-11-26 12:27:33 -0500

    Login

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,10 @@ namespace Proy_A_Rent.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
