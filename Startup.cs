12c5105e7c05178373505b8086a83a42538aee5a
Raí Campos <raicampas23@gmail.com>
2020-12-03 15:38:39 -0500

    db commit

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Proy_A_Rent.Data;
+using Microsoft.EntityFrameworkCore;
 using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 namespace Proy_A_Rent
